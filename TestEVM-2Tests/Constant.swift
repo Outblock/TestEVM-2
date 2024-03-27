1c4dfb720bbc140a6bc8a8248c8c3f98cdfa32fd
lmcmz <lmcmze@gmail.com>
2024-03-27 23:58:19 +1100

    Clean up the code

---
+++ TestEVM-2Tests/Constant.swift
@@ -0,0 +1,261 @@
+//
+//  Constant.swift
+//  TestEVM-2Tests
+//
+//  Created by Hao Fu on 27/3/2024.
+//
+
+import Foundation
+import WalletCore
+import web3swift
+import Web3Core
+import Flow
+import BigInt
+
+let flowPath = "m/44'/539'/0'/0/0"
+
+struct Signer: FlowSigner {
+    var address: Flow.Address
+    var keyIndex: Int = 0
+    
+    var hdWallet: HDWallet
+    
+    func sign(transaction: Flow.Transaction, signableData: Data) async throws -> Data {
+        let hashed = Hash.sha256(data: signableData)
+        let pk = hdWallet.getKeyByCurve(curve: .secp256k1, derivationPath: flowPath)
+        return pk.sign(digest: hashed, curve: .secp256k1)!.dropLast()
+    }
+}
+
+protocol RLPEncodable {
+    var rlpList: [Any] { get }
+}
+
+extension RLPEncodable {
+    var rlpList : [Any] {
+        let mirror = Mirror(reflecting: self)
+        return mirror.children.compactMap { $0.value }
+    }
+}
+
+struct COAOwnershipProof: RLPEncodable {
+    let keyIninces: [BigUInt]
+    let address: Data
+    let capabilityPath: String
+    let signatures: [Data]
+}
+
+let coaABI = """
+[
+    {
+        "inputs": [],
+        "name": "cadenceArch",
+        "outputs": [
+            {
+                "internalType": "address",
+                "name": "",
+                "type": "address"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "inputs": [
+            {
+                "internalType": "bytes32",
+                "name": "_hash",
+                "type": "bytes32"
+            },
+            {
+                "internalType": "bytes",
+                "name": "_sig",
+                "type": "bytes"
+            }
+        ],
+        "name": "isValidSignature",
+        "outputs": [
+            {
+                "internalType": "bytes4",
+                "name": "",
+                "type": "bytes4"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "inputs": [
+            {
+                "internalType": "address",
+                "name": "",
+                "type": "address"
+            },
+            {
+                "internalType": "address",
+                "name": "",
+                "type": "address"
+            },
+            {
+                "internalType": "uint256[]",
+                "name": "",
+                "type": "uint256[]"
+            },
+            {
+                "internalType": "uint256[]",
+                "name": "",
+                "type": "uint256[]"
+            },
+            {
+                "internalType": "bytes",
+                "name": "",
+                "type": "bytes"
+            }
+        ],
+        "name": "onERC1155BatchReceived",
+        "outputs": [
+            {
+                "internalType": "bytes4",
+                "name": "",
+                "type": "bytes4"
+            }
+        ],
+        "stateMutability": "pure",
+        "type": "function"
+    },
+    {
+        "inputs": [
+            {
+                "internalType": "address",
+                "name": "",
+                "type": "address"
+            },
+            {
+                "internalType": "address",
+                "name": "",
+                "type": "address"
+            },
+            {
+                "internalType": "uint256",
+                "name": "",
+                "type": "uint256"
+            },
+            {
+                "internalType": "uint256",
+                "name": "",
+                "type": "uint256"
+            },
+            {
+                "internalType": "bytes",
+                "name": "",
+                "type": "bytes"
+            }
+        ],
+        "name": "onERC1155Received",
+        "outputs": [
+            {
+                "internalType": "bytes4",
+                "name": "",
+                "type": "bytes4"
+            }
+        ],
+        "stateMutability": "pure",
+        "type": "function"
+    },
+    {
+        "inputs": [
+            {
+                "internalType": "address",
+                "name": "",
+                "type": "address"
+            },
+            {
+                "internalType": "address",
+                "name": "",
+                "type": "address"
+            },
+            {
+                "internalType": "uint256",
+                "name": "",
+                "type": "uint256"
+            },
+            {
+                "internalType": "bytes",
+                "name": "",
+                "type": "bytes"
+            }
+        ],
+        "name": "onERC721Received",
+        "outputs": [
+            {
+                "internalType": "bytes4",
+                "name": "",
+                "type": "bytes4"
+            }
+        ],
+        "stateMutability": "pure",
+        "type": "function"
+    },
+    {
+        "inputs": [
+            {
+                "internalType": "bytes4",
+                "name": "id",
+                "type": "bytes4"
+            }
+        ],
+        "name": "supportsInterface",
+        "outputs": [
+            {
+                "internalType": "bool",
+                "name": "",
+                "type": "bool"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "inputs": [
+            {
+                "internalType": "address",
+                "name": "",
+                "type": "address"
+            },
+            {
+                "internalType": "address",
+                "name": "",
+                "type": "address"
+            },
+            {
+                "internalType": "address",
+                "name": "",
+                "type": "address"
+            },
+            {
+                "internalType": "uint256",
+                "name": "",
+                "type": "uint256"
+            },
+            {
+                "internalType": "bytes",
+                "name": "",
+                "type": "bytes"
+            },
+            {
+                "internalType": "bytes",
+                "name": "",
+                "type": "bytes"
+            }
+        ],
+        "name": "tokensReceived",
+        "outputs": [],
+        "stateMutability": "pure",
+        "type": "function"
+    },
+    {
+        "stateMutability": "payable",
+        "type": "receive"
+    }
+]
+"""
--- TestEVM-2Tests/TestEVM_2Tests.swift
+++ TestEVM-2Tests/TestEVM_2Tests.swift
@@ -13,46 +13,8 @@ import Web3Core
 import Flow
 import BigInt
 
-let flowPath = "m/44'/539'/0'/0/0"
-
-struct Signer: FlowSigner {
-    var address: Flow.Address
-    var keyIndex: Int = 0
-    
-    var hdWallet: HDWallet
-    
-    func sign(transaction: Flow.Transaction, signableData: Data) async throws -> Data {
-        let hashed = Hash.sha256(data: signableData)
-        let pk = hdWallet.getKeyByCurve(curve: .secp256k1, derivationPath: flowPath)
-        return pk.sign(digest: hashed, curve: .secp256k1)!.dropLast()
-    }
-}
-
-protocol RLPEncodable {
-    var rlpList: [Any] { get }
-}
-
-extension RLPEncodable {
-    var rlpList : [Any] {
-        let mirror = Mirror(reflecting: self)
-        return mirror.children.compactMap { $0.value }
-    }
-}
-
-struct COAOwnershipProof: RLPEncodable {
-    let keyIninces: [BigUInt]
-    let address: Data
-    let capabilityPath: String
-    let signatures: [Data]
-}
-
 
 final class TestEVM_2Tests: XCTestCase {
-    
-//    override class func setUp() {
-//        let provider = try! await Web3HttpProvider(url: URL(string: "https://previewnet.evm.nodes.onflow.org")!, network: .Custom(networkID: 646))
-//        let web3 = Web3(provider: provider)
-//    }
     
     var provider: Web3HttpProvider?
     var web3: Web3?
@@ -158,9 +120,6 @@ final class TestEVM_2Tests: XCTestCase {
     }
     
     func testSignMessage() async throws {
-//        let hdWallet = HDWallet(mnemonic: "marriage pipe hamster army often include biology banana nose clutch damage helmet", passphrase: "")
-//        let code = try! await web3!.eth.code(for: EthereumAddress.init("0x000000000000000000000002a7D95998765430e7")!)
-        
         let text = "this is a message"
         guard let textData = text.data(using: .utf8) else {
             return
@@ -170,228 +129,12 @@ final class TestEVM_2Tests: XCTestCase {
         let signableData = Flow.DomainTag.user.normalize + textData
         let pk = hdWallet.getKeyByCurve(curve: .secp256k1, derivationPath: flowPath)
         let hashed = Hash.sha256(data: signableData)
-        
         let hashed2 = Hash.sha256(data: textData)
         let sig = pk.sign(digest: hashed, curve: .secp256k1)!.dropLast()
         
-        let abi = """
-        [
-            {
-                "inputs": [],
-                "name": "cadenceArch",
-                "outputs": [
-                    {
-                        "internalType": "address",
-                        "name": "",
-                        "type": "address"
-                    }
-                ],
-                "stateMutability": "view",
-                "type": "function"
-            },
-            {
-                "inputs": [
-                    {
-                        "internalType": "bytes32",
-                        "name": "_hash",
-                        "type": "bytes32"
-                    },
-                    {
-                        "internalType": "bytes",
-                        "name": "_sig",
-                        "type": "bytes"
-                    }
-                ],
-                "name": "isValidSignature",
-                "outputs": [
-                    {
-                        "internalType": "bytes4",
-                        "name": "",
-                        "type": "bytes4"
-                    }
-                ],
-                "stateMutability": "view",
-                "type": "function"
-            },
-            {
-                "inputs": [
-                    {
-                        "internalType": "address",
-                        "name": "",
-                        "type": "address"
-                    },
-                    {
-                        "internalType": "address",
-                        "name": "",
-                        "type": "address"
-                    },
-                    {
-                        "internalType": "uint256[]",
-                        "name": "",
-                        "type": "uint256[]"
-                    },
-                    {
-                        "internalType": "uint256[]",
-                        "name": "",
-                        "type": "uint256[]"
-                    },
-                    {
-                        "internalType": "bytes",
-                        "name": "",
-                        "type": "bytes"
-                    }
-                ],
-                "name": "onERC1155BatchReceived",
-                "outputs": [
-                    {
-                        "internalType": "bytes4",
-                        "name": "",
-                        "type": "bytes4"
-                    }
-                ],
-                "stateMutability": "pure",
-                "type": "function"
-            },
-            {
-                "inputs": [
-                    {
-                        "internalType": "address",
-                        "name": "",
-                        "type": "address"
-                    },
-                    {
-                        "internalType": "address",
-                        "name": "",
-                        "type": "address"
-                    },
-                    {
-                        "internalType": "uint256",
-                        "name": "",
-                        "type": "uint256"
-                    },
-                    {
-                        "internalType": "uint256",
-                        "name": "",
-                        "type": "uint256"
-                    },
-                    {
-                        "internalType": "bytes",
-                        "name": "",
-                        "type": "bytes"
-                    }
-                ],
-                "name": "onERC1155Received",
-                "outputs": [
-                    {
-                        "internalType": "bytes4",
-                        "name": "",
-                        "type": "bytes4"
-                    }
-                ],
-                "stateMutability": "pure",
-                "type": "function"
-            },
-            {
-                "inputs": [
-                    {
-                        "internalType": "address",
-                        "name": "",
-                        "type": "address"
-                    },
-                    {
-                        "internalType": "address",
-                        "name": "",
-                        "type": "address"
-                    },
-                    {
-                        "internalType": "uint256",
-                        "name": "",
-                        "type": "uint256"
-                    },
-                    {
-                        "internalType": "bytes",
-                        "name": "",
-                        "type": "bytes"
-                    }
-                ],
-                "name": "onERC721Received",
-                "outputs": [
-                    {
-                        "internalType": "bytes4",
-                        "name": "",
-                        "type": "bytes4"
-                    }
-                ],
-                "stateMutability": "pure",
-                "type": "function"
-            },
-            {
-                "inputs": [
-                    {
-                        "internalType": "bytes4",
-                        "name": "id",
-                        "type": "bytes4"
-                    }
-                ],
-                "name": "supportsInterface",
-                "outputs": [
-                    {
-                        "internalType": "bool",
-                        "name": "",
-                        "type": "bool"
-                    }
-                ],
-                "stateMutability": "view",
-                "type": "function"
-            },
-            {
-                "inputs": [
-                    {
-                        "internalType": "address",
-                        "name": "",
-                        "type": "address"
-                    },
-                    {
-                        "internalType": "address",
-                        "name": "",
-                        "type": "address"
-                    },
-                    {
-                        "internalType": "address",
-                        "name": "",
-                        "type": "address"
-                    },
-                    {
-                        "internalType": "uint256",
-                        "name": "",
-                        "type": "uint256"
-                    },
-                    {
-                        "internalType": "bytes",
-                        "name": "",
-                        "type": "bytes"
-                    },
-                    {
-                        "internalType": "bytes",
-                        "name": "",
-                        "type": "bytes"
-                    }
-                ],
-                "name": "tokensReceived",
-                "outputs": [],
-                "stateMutability": "pure",
-                "type": "function"
-            },
-            {
-                "stateMutability": "payable",
-                "type": "receive"
-            }
-        ]
-        """
-        
         let proof = COAOwnershipProof(keyIninces: [0], address: address.data, capabilityPath: "coa", signatures: [sig])
         let encoded = RLP.encode(proof.rlpList)!
-        let contract = web3!.contract(abi, at: ethAddress)!
+        let contract = web3!.contract(coaABI, at: ethAddress)!
 //        print(contract.contract.methods.keys)
         print("pubK ===> \(pk.getPublicKeySecp256k1(compressed: false).data.hexValue)")
         print("encoded ===> \(encoded.hexValue)")
@@ -411,7 +154,6 @@ final class TestEVM_2Tests: XCTestCase {
         }
         
         let verfy = pk.getPublicKeySecp256k1(compressed: false).verify(signature: sig, message: hashed)
-        
         print("verfy ==> \(verfy)")
         
         print(response)
