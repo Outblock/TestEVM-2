77f85c21906b8f279fe1e6fab349a20bb08fd404
lmcmz <lmcmze@gmail.com>
2024-03-28 12:10:30 +1100

    Upgrade script

--- TestEVM-2Tests/TestEVM_2Tests.swift
+++ TestEVM-2Tests/TestEVM_2Tests.swift
@@ -16,23 +16,23 @@ import BigInt
 
 final class TestEVM_2Tests: XCTestCase {
     
-    var provider: Web3HttpProvider?
-    var web3: Web3?
+    var provider: Web3HttpProvider!
+    var web3: Web3!
     var hdWallet: HDWallet!
-    let address = Flow.Address(hex: "0xc3f180fad698d157")
-    let ethAddress = EthereumAddress("0x000000000000000000000002a7D95998765430e7")!
+    let address = Flow.Address(hex: "0xd962e1938ab387c8")
+    let ethAddress = EthereumAddress("0x0000000000000000000000029a9d22fe53a8fc9f")!
     
     let magicValue = "0x1626ba7e"
     
     override func setUp() async throws {
         provider = try! await Web3HttpProvider(url: URL(string: "https://previewnet.evm.nodes.onflow.org")!, network: .Custom(networkID: 646))
-        web3 = Web3(provider: provider!)
+        web3 = Web3(provider: provider)
         flow.configure(chainID: .custom(name: "previewnet", transport: .HTTP(URL(string: "https://rest-previewnet.onflow.org")!)))
-        hdWallet = HDWallet(mnemonic: "marriage pipe hamster army often include biology banana nose clutch damage helmet", passphrase: "")!
+        hdWallet = HDWallet(mnemonic: "kiwi erosion weather slam harvest move crumble zero juice steel start hotel", passphrase: "")!
     }
     
     func testExample() async throws {
-        let code = try! await web3!.eth.code(for: EthereumAddress.init("0x000000000000000000000002a48e1e98cbff3194")!)
+        let code = try! await web3.eth.code(for: EthereumAddress.init("0x000000000000000000000002a48e1e98cbff3194")!)
         print(code)
     }
     
@@ -77,27 +77,30 @@ final class TestEVM_2Tests: XCTestCase {
                 import FlowToken from 0x4445e7ad11568276
                 import EVM from 0xb6763b4399a888c8
 
-
                 transaction(amount: UFix64) {
                     let sentVault: @FlowToken.Vault
-                    let auth: auth(Storage) &Account
+                    let auth: auth(IssueStorageCapabilityController, IssueStorageCapabilityController, PublishCapability, SaveValue, UnpublishCapability) &Account
 
-                    prepare(signer: auth(Storage) &Account) {
+                    prepare(signer: auth(BorrowValue, IssueStorageCapabilityController, PublishCapability, SaveValue, UnpublishCapability) &Account) {
                         let vaultRef = signer.storage.borrow<auth(FungibleToken.Withdraw) &FlowToken.Vault>(
-                            from: /storage/flowTokenVault
-                        ) ?? panic("Could not borrow reference to the owner's Vault!")
+                                from: /storage/flowTokenVault
+                            ) ?? panic("Could not borrow reference to the owner's Vault!")
 
                         self.sentVault <- vaultRef.withdraw(amount: amount) as! @FlowToken.Vault
                         self.auth = signer
                     }
 
                     execute {
-                        let account <- EVM.createCadenceOwnedAccount()
-                        log(account.address())
-                        account.deposit(from: <-self.sentVault)
+                        let coa <- EVM.createCadenceOwnedAccount()
+                        coa.deposit(from: <-self.sentVault)
 
-                        log(account.balance())
-                        self.auth.storage.save<@EVM.CadenceOwnedAccount>(<-account, to: StoragePath(identifier: "evm")!)
+                        log(coa.balance().inFLOW())
+                        let storagePath = StoragePath(identifier: "evm")!
+                        let publicPath = PublicPath(identifier: "evm")!
+                        self.auth.storage.save<@EVM.CadenceOwnedAccount>(<-coa, to: storagePath)
+                        let addressableCap = self.auth.capabilities.storage.issue<&EVM.CadenceOwnedAccount>(storagePath)
+                        self.auth.capabilities.unpublish(publicPath)
+                        self.auth.capabilities.publish(addressableCap, at: publicPath)
                     }
                 }
                 """
@@ -120,22 +123,21 @@ final class TestEVM_2Tests: XCTestCase {
     }
     
     func testSignMessage() async throws {
-        let text = "this is a message"
-        guard let textData = text.data(using: .utf8) else {
-            return
-        }
+//        let text = "this is a message"
+//        guard let textData = text.data(using: .utf8) else {
+//            return
+//        }
         
-//        let textData = Data.randomBytes(length: 32)!
+        let textData = Data.randomBytes(length: 32)!
         let signableData = Flow.DomainTag.user.normalize + textData
         let pk = hdWallet.getKeyByCurve(curve: .secp256k1, derivationPath: flowPath)
         let hashed = Hash.sha256(data: signableData)
         let hashed2 = Hash.sha256(data: textData)
         let sig = pk.sign(digest: hashed, curve: .secp256k1)!.dropLast()
         
-        let proof = COAOwnershipProof(keyIninces: [0], address: address.data, capabilityPath: "coa", signatures: [sig])
+        let proof = COAOwnershipProof(keyIninces: [0], address: address.data, capabilityPath: "/public/evm", signatures: [sig])
         let encoded = RLP.encode(proof.rlpList)!
-        let contract = web3!.contract(coaABI, at: ethAddress)!
-//        print(contract.contract.methods.keys)
+        let contract = web3.contract(coaABI, at: ethAddress)!
         print("pubK ===> \(pk.getPublicKeySecp256k1(compressed: false).data.hexValue)")
         print("encoded ===> \(encoded.hexValue)")
         print("flow address ===> \(address)")
@@ -145,7 +147,7 @@ final class TestEVM_2Tests: XCTestCase {
         print("hashed message ===> \(hashed.hexString)")
         print("sig ===> \(sig.hexString)")
         print("encoded ===> \(encoded.hexString)")
-        let read = contract.createReadOperation("isValidSignature", parameters: [hashed, encoded])!
+        let read = contract.createReadOperation("isValidSignature", parameters: [hashed2, encoded])!
 //        read.transaction.from = ethAddress
         let response = try await read.callContractMethod()
         
@@ -153,10 +155,12 @@ final class TestEVM_2Tests: XCTestCase {
             return
         }
         
-        let verfy = pk.getPublicKeySecp256k1(compressed: false).verify(signature: sig, message: hashed)
-        print("verfy ==> \(verfy)")
+//        let verfy = pk.getPublicKeySecp256k1(compressed: false).verify(signature: sig, message: hashed)
+//        print("verfy ==> \(verfy)")
         
         print(response)
         print(data.hexValue)
+        
+        XCTAssertEqual(data, Data(hex: magicValue))
     }
 }
